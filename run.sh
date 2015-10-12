1ae8ef4f319db9211f095b04048256fd9fdc09aa
g-div <g-div@anche.no>
2015-10-12 15:24:59 +0200

    update to ubuntu 14.04 and move the snapshot to Frankfurt 1

--- run.sh
+++ run.sh
@@ -5,20 +5,11 @@ PACKER_BIN="packer-io"
 PACKER_BUILD="build.json"
 SSH_PUB_KEY="$HOME/.ssh/id_rsa.pub"
 
-BASEDIR=$(dirname $0)
-
-cat $SSH_PUB_KEY > $BASEDIR/ansible/files/id_rsa.pub
-
-# This script will delete all symlinks in this folder.
-# Ir's a quickfix for the following bug:
-# https://github.com/mitchellh/packer/issues/1627
-cd $BASEDIR
-find -type l -delete
+cat $SSH_PUB_KEY > ./ansible/files/id_rsa.pub
 
 $PACKER_BIN validate $PACKER_BUILD
 $PACKER_BIN build $PACKER_BUILD
 
 unset PACKER_BIN
 unset PACKER_BUILD
-unset BASEDIR
 unset SSH_PUB_KEY
